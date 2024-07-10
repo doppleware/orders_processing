d1142693275b9cb8c1c4bc86689fef6637289dcb
asm0dey <pavel.finkelshtein@gmail.com>
2024-07-10 12:44:36 +0200

    Adds equals/hashCode implementations + liquibase migration

--- order_processing/src/main/java/com/acme/orders/order_processing/domain/BillingEntity.java
+++ order_processing/src/main/java/com/acme/orders/order_processing/domain/BillingEntity.java
@@ -1,9 +1,12 @@
 package com.acme.orders.order_processing.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.proxy.HibernateProxy;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
+
 @Entity
 @Table(name = "billing_entities")
 public class BillingEntity extends BaseEntity{
@@ -29,4 +32,22 @@ public class BillingEntity extends BaseEntity{
 
     @Column(name = "description")
     private String description;
+
+    //<editor-fold desc="equals and hashCode">
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        BillingEntity that = (BillingEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+    //</editor-fold>
 }
